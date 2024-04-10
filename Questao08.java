9b76fb492fcfb1503646c6b979c0c73719cdf15b
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-04-09 22:30:34 -0300

    Update and rename

--- Questao08.java
+++ Questao08.java
@@ -1,20 +1,21 @@
-/* 
-  ALUNA: VITÓRIA BARBOSA GOMES SOARES
-  
-  A sequência Collatz mais larga
-  
-  - A seguinte sequência iterativa esta definida pelo conjunto de inteiros positivos:
-      n-> n/2 (n é par)
-      n->3n + 1(n é ímpar)
-  
-  - Usando a regra acima e começando com 13, geramos a seguinte sequência:
-      13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1​
-      Pode-se ver que esta sequência (começando em 13 e terminando em 1) contém 10 termos, e 9 passos.
-  
-  Implemente um programa que mostre o número com a cadeia mais larga, o número de passos, e a sequência debaixo de um determinado limite. Exemplo: Limite: 10,  Número:9 , Passos: 19, sequencia: ....  
-*/
+/*
+ * VITÓRIA BARBOSA GOMES SOARES
+ * 
+ * A sequência Collatz mais larga
+ * 
+ * - A seguinte sequência iterativa esta definida pelo conjunto de inteiros positivos:
+ *      n-> n/2 (n é par)
+ *      n->3n + 1(n é ímpar)
+ * 
+ * - Usando a regra acima e começando com 13, geramos a seguinte sequência:
+ *      13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1​
+ *      Pode-se ver que esta sequência (começando em 13 e terminando em 1) contém 10 termos, e 9 passos.
+ * 
+ * Implemente um programa que mostre o número com a cadeia mais larga, o número de passos, e a sequência debaixo de um determinado limite. 
+ *      Exemplo: Limite: 10,  Número:9 , Passos: 19, sequencia: ....  
+ */
 
-public class Questao8 {
+public class Questao08 {
     public static void main(String[] args) {
         int limite = 10;
         int maiorCadeia = 0;
