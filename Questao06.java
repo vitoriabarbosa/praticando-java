45bc98617021abcbf2f7e3c39fe493103dd03fad
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-04-09 22:24:34 -0300

    Update and rename

--- Questao06.java
+++ Questao06.java
@@ -1,26 +1,26 @@
-// VITÓRIA BARBOSA GOMES SOARES
-
-/* 
-  Desenvolva um programa que mostre um menu de opções. 
-  Quando o usuário escolhe uma opção, o preço equivalente desta deve ser somado ào anterior e o menu deve ser mostrado novamente. 
-  O menu só para de ser exibido quando o usuário escolhe a opção SAIR. Quando o usuário escolher essa opção, o programa deve mostrar o valor total da compra. 
-    Exemplo:
-    O usuário começa com a variável compras = 0 usd
-    
-    1. Arroz- 8 usd
-    2. Feijao - 10 usd
-    3. Batata - 15 usd
-    4. Carne - 100 usd
-    0. Sair
-    "Ingresse com uma opção: "
-    
-    Suponha que o usuário digitou 2
-    Total da Compra: 10usd
-*/
+/*
+ * VITÓRIA BARBOSA GOMES SOARES
+ * 
+ * Desenvolva um programa que mostre um menu de opções. 
+ * Quando o usuário escolhe uma opção, o preço equivalente desta deve ser somado ào anterior e o menu deve ser mostrado novamente. 
+ * O menu só para de ser exibido quando o usuário escolhe a opção SAIR. Quando o usuário escolher essa opção, o programa deve mostrar o valor total da compra. 
+ * 
+ *  Exemplo:
+ *  O usuário começa com a variável compras = 0 usd
+ *      1. Arroz- 8 usd
+ *      2. Feijao - 10 usd
+ *      3. Batata - 15 usd
+ *      4. Carne - 100 usd
+ *      0. Sair
+ * 
+ *  "Ingresse com uma opção: "
+ * 
+ *  Suponha que o usuário digitou 2
+ *  Total da Compra: 10usd
+ */
 
 import java.util.Scanner;
-
-public class Questao6 {
+public class Questao06 {
     public static void main(String[] args) {      
         Scanner scanner = new Scanner(System.in);           
         boolean continuar = true;
@@ -31,15 +31,15 @@ public class Questao6 {
                 -------------------------
                           MENU
                 -------------------------
-                1. Arroz    -   8 usd
+                 1. Arroz    -   8 usd
                 -------------------------
-                2. Feijao   -   10 usd
+                 2. Feijao   -   10 usd
                 -------------------------
-                3. Batata   -   15 usd
+                 3. Batata   -   15 usd
                 -------------------------
-                4. Carne    -   100 usd
+                 4. Carne    -   100 usd
                 -------------------------
-                        0. Sair
+                         0. Sair
                 -------------------------
                 """;
                 
