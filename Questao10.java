5310dbaf0ebfb74d1ca9e541f8063c43df2c31aa
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-04-09 22:38:33 -0300

    Update Questao10.java

--- Questao10.java
+++ Questao10.java
@@ -1,13 +1,12 @@
-/* 
-VITÓRIA BARBOSA GOMES SOARES
-
-Desenvolva um programa para multiplicar dois vetores do mesmo tamanho. Os resultados devem ser armazenados em um vetor resultante.
-    Por exemplo:
-
-    v1: {1,3,4,5}
-    v2: {1,4,5,6}
-    v-resultado:{1,12,20,30}
-*/
+/*
+ * VITÓRIA BARBOSA GOMES SOARES
+ * 
+ * Desenvolva um programa para multiplicar dois vetores do mesmo tamanho. Os resultados devem ser armazenados em um vetor resultante.
+ * Por exemplo:
+ *     v1: {1,3,4,5}
+ *     v2: {1,4,5,6}
+ *     v-resultado: {1,12,20,30}
+ */
 
 public class Questao10 {
     public static void main(String[] args) {
@@ -24,11 +23,13 @@ public class Questao10 {
         System.out.print("\nMultiplicação dos vetores: ");
         imprimir(resultado);
     }
+    
     static void imprimir(int[] vetor) {
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(vetor[i] + " ");
         } System.out.println();
     }
+    
     static void multiplicarVetores(int[] vetor1, int[] vetor2, int[] resultado) {
         for (int i = 0; i < vetor1.length; i++) {
             resultado[i] = vetor1[i] * vetor2[i];
