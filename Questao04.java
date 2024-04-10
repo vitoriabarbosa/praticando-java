e67406a7dbeb659a18b7e4e6f12502b986167da7
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2024-04-09 21:55:42 -0300

    Update and rename

--- Questao04.java
+++ Questao04.java
@@ -1,27 +1,25 @@
-// VITÓRIA BARBOSA GOMES SOARES
-
-/* 
-QUESTÃO 4 - Implemente um programa, que permita ao usuário ingressar os dados de dureza, teor de carvão 
-e a resistência da tração e como saída mostre a pontuação do aço. (Resolva com if-else)
-
-    - O grau do aço é obtido em base às seguintes condições:
-    Dureza deve ser maior que 50
-    Teor de Carvão deve ser menor que 0.7
-    Resistencia à tração deve ser maior que 5600
-
-    - As pontuações são obtidas da seguinte forma:
-    Pontuação é 10 se todas as condições são cumpridas
-    Pontuação é 9 se as condições (i) e (ii) são cumpridas
-    Pontuação é 8 se as condições (ii) e (iii) cumpridas
-    Pontuação é 7 se as condições (i) e (iii) cumpridas
-    Pontuação é 6 se somente uma condição é cumprida
-    Pontuação é 5 se nenhuma das condições são cumpridas
-*/
-
+/*
+ * VITÓRIA BARBOSA GOMES SOARES
+ * 
+ * Implemente um programa, que permita ao usuário ingressar os dados de dureza, teor de carvão e a resistência da tração e como saída mostre a pontuação do aço. 
+ * (Resolva com if-else)
+ * 
+ *  - O grau do aço é obtido em base às seguintes condições:
+ *      Dureza deve ser maior que 50
+ *      Teor de Carvão deve ser menor que 0.7
+ *      Resistencia à tração deve ser maior que 5600
+ *  
+ *  - As pontuações são obtidas da seguinte forma:
+ *      Pontuação é 10 se todas as condições são cumpridas
+ *      Pontuação é 9 se as condições (i) e (ii) são cumpridas
+ *      Pontuação é 8 se as condições (ii) e (iii) cumpridas
+ *      Pontuação é 7 se as condições (i) e (iii) cumpridas
+ *      Pontuação é 6 se somente uma condição é cumprida
+ *      Pontuação é 5 se nenhuma das condições são cumpridas
+ */
 
 import java.util.Scanner;
-
-public class Questao4 {    
+public class Questao04 {    
     public static void main(String[] args){
         System.out.println("\n--------------------------------------------------\n");
         System.out.println("Questão 4 - IF-ELSE\n\n");
